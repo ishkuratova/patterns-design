483b073e873e10963c17fec280bcbf73f62a1de0
ishkuratova <i.shkuratova.kubsu@gmail.com>
2025-11-28 19:04:22 +0300

    дополнение функционалаorganization_controller.py
    добавлена функция удаления

--- task3/controllers/organization_controller.py
+++ task3/controllers/organization_controller.py
@@ -1,29 +1,25 @@
 from flask import current_app
 from models.reps import EmployeeRepJson, EmployeeRepYaml, EmployeeRepDBAdapter
-from models.observable_repository import ObservableEmployeeRepository
+
 
 class OrganizationController:
     """Контроллер для управления организациями и их репозиториями"""
-    
+
     def __init__(self):
         self.organizations = current_app.config['ORGANIZATIONS']
         self._repositories = {}
-        self._observable_repositories = {}
         self._initialize_repositories()
-    
+
     def _initialize_repositories(self):
         """Инициализация репозиториев для всех организаций"""
         for org_id, org_config in self.organizations.items():
             repo = self._create_repository(org_id, org_config)
-            observable_repo = ObservableEmployeeRepository(repo)
-            
             self._repositories[org_id] = repo
-            self._observable_repositories[org_id] = observable_repo
-    
+
     def _create_repository(self, org_id, org_config):
         """Создание репозитория по типу хранилища"""
         repo_type = org_config['type']
-        
+
         if repo_type == 'json':
             return EmployeeRepJson(org_config['file_path'])
         elif repo_type == 'yaml':
@@ -39,19 +35,15 @@ class OrganizationController:
             )
         else:
             raise ValueError(f"Unknown repository type: {repo_type}")
-    
+
     def get_repository(self, organization_id):
         """Получение основного репозитория по ID организации"""
         return self._repositories.get(organization_id)
-    
-    def get_observable_repository(self, organization_id):
-        """Получение наблюдаемого репозитория по ID организации"""
-        return self._observable_repositories.get(organization_id)
-    
+
     def get_all_organizations(self):
         """Получение списка всех организаций"""
         return self.organizations
-    
+
     def get_organization_name(self, organization_id):
         """Получение названия организации по ID"""
         org = self.organizations.get(organization_id)
